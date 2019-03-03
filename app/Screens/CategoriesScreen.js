c1bd3c8e06faa23fd29f818f1898fed7e4d447ed
Andy <anlaics2@gmail.com>
2019-03-02 18:47:10 -0800

    pass in categoryid for a list of products

--- app/Screens/CategoriesScreen.js
+++ app/Screens/CategoriesScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 import { connect } from 'react-redux';
 
@@ -10,13 +10,23 @@ class CategoriesScreen extends React.Component {
     this.loadStations();
   }
   loadStations = () => {
-    this.props.fetchStations(() => {});
+    this.props.fetchStations(22050, () => {});
   };
+
+  renderCategories() {
+    console.log(this.props.categories.length);
+    return this.props.categories[0].product.product.map((data, i) => {
+      console.log(data.summary.name);
+      <Text>{data}</Text>
+    })
+  }
   render() {
-    console.log(this.props.categories);
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Categories!</Text>
+        <ScrollView>
+        {this.props.categories.length > 0 ? this.renderCategories() : null}
+        </ScrollView>
       </View>
     );
   }
