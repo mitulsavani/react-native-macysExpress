5d6355d9724ccba419043e29d637830d50f3b231
Andy <anlaics2@gmail.com>
2019-03-02 18:52:56 -0800

    fix build errors

--- app/Screens/CategoriesScreen.js
+++ app/Screens/CategoriesScreen.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Text, View, ScrollView, Button } from 'react-native';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
-import { Text, View, Button } from 'react-native';
 import { connect } from 'react-redux';
 
 import * as actions from '../actions';
@@ -14,20 +13,10 @@ class CategoriesScreen extends React.Component {
     this.props.fetchStations(22050, () => {});
   };
 
-  renderCategories() {
-    console.log(this.props.categories.length);
-    return this.props.categories[0].product.product.map((data, i) => {
-      console.log(data.summary.name);
-      <Text>{data}</Text>
-    })
-  }
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Categories!</Text>
-        <ScrollView>
-        {this.props.categories.length > 0 ? this.renderCategories() : null}
-        </ScrollView>
         <Button
         title="Products Screen"
           onPress={() => this.props.navigation.navigate('Products')}
